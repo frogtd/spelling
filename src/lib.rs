bc0b40ffb00049176b60293ea1dccd78c10c7549
frogtd <31412003+frogtd@users.noreply.github.com>
2021-04-06 22:25:37 -0400

    Docs + other things
    find a bug with not subtracting 1 from index
    fix formatting
    add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,79 @@
 #![feature(test)]
 
-
 use rayon::prelude::*;
-
-
 
 /// Takes a `dictionary_string` (newline separated), a word and a distance and
 /// returns a vector of possible matches, with a limit of distance set up
-/// `distance`.
-/// Note: this only works for single length code bytes
+/// `distance`. Sorts by distance. 
+/// 
+/// Notes:
+/// 1. This only works for single length code bytes.
+/// 2. This uses the Levenshtein distance
+///
 /// How it works:
 ///
 /// It loops through each word in the dictionary and then you have a word from
 /// the dictionary and a word to match against.
 ///
 /// Lets say those words were `rumor` and `rotten`. You then create a vector
-/// that is the length of the shorter word + 1, counting up.
+/// that is the length of the shorter word + 1, counting up (but the first value
+/// is one).
 /// ```
-/// let mut list = vec![0, 1, 2, 3, 4, 5];
+/// let mut list = vec![1, 1, 2, 3, 4, 5];
 /// ```
 /// You also keep track a left variable, which starts at the index of the
-/// longest string you're at.
+/// longest string you're at and you create a temporary row.
 /// ```
-/// # let longest_word = "rotten";
-/// for x in 2..longest_word.len() {
+/// # let longer = "rotten";
+/// for x in 2..longer.len() {
 ///     let mut left = x;
+///     let mut temp = Vec::new();
+///     # temp.push(10 as usize)
+/// }
+/// ```
+/// For every item in the list, except the first, check if letter you're on 
+/// is the same in both words. If it is left will become list at the current
+/// index you're on minus one. If it isn't left will become the minimum of 
+/// the current index on `list`, the current index minus on `list`, and left. 
+/// Then add `left` to `temp`.
+/// 
+/// After this set `list` to `temp` and repeat.
+/// 
+/// The distance will be in the last index of list at the end, so return that
+/// to filter.
+///
+/// ```
+/// # let longer = "rotten";
+/// # let shorter = "rumor";
+/// # let mut list: Vec<usize> = Vec::with_capacity(shorter.len() + 1);
+/// # list.push(1);
+/// # for index in 1..(shorter.len() + 1) {
+/// #     list.push(index) 
+/// # }
+/// for x in 2..longer.len() {
+///     let mut left = x;
+///     println!("{}", x);
+///     let mut temp = Vec::new();
+///     temp.push(left);
+///     let mut iter = list.iter().enumerate();
+///     iter.next(); // skip first item in list
+///     for (index, y) in iter {
+///         println!("{}", index);
+///         left = match longer.as_bytes()[x - 1] == shorter.as_bytes()[index - 1] {
+///             true => list[index - 1],
+///             false => [list[index - 1], *y, left].iter().min().unwrap() + 1,
+///         };
+///         temp.push(left)
+///     }
+///     list = temp;
 /// }
 /// ```
 ///
-pub fn spellcheck<'a>(dictionary_string: &'a str, word: &str, distance: usize) -> Vec<&'a str>  {
+pub fn spellcheck<'a>(dictionary_string: &'a str, word: &str, distance: usize) -> Vec<&'a str> {
     let mut vec: Vec<_> = dictionary_string
         .split('\n')
         .collect::<Vec<&str>>()
-        .par_iter()
+        .iter()
         .map(|string_in| {
             let (shorter, longer) = {
                 if string_in.len() > word.len() {
@@ -42,27 +83,30 @@ pub fn spellcheck<'a>(dictionary_string: &'a str, word: &str, distance: usize) -
                 }
             };
 
-            let mut list: Vec<usize> = Vec::with_capacity(string_in.len()+1);
+            // create list like [1, 1, 2, 3, 4, 5];
+            let mut list: Vec<usize> = Vec::with_capacity(shorter.len() + 1);
             list.push(1);
-            for index in 1..shorter.len() {
+            for index in 1..(shorter.len() + 1) {
                 list.push(index)
-            };
+            }
 
             for x in 2..(longer.len() + 1) {
                 let mut left = x;
-                let mut temp: Vec<usize> = Vec::with_capacity(shorter.len()+1);
+                let mut temp: Vec<usize> = Vec::with_capacity(shorter.len() + 1);
                 temp.push(left);
                 let mut iter = list.iter().enumerate();
                 iter.next();
                 for (index, y) in iter {
-                    left = match longer.as_bytes()[x-1] == shorter.as_bytes()[index] {
-                        true => list[index-1],
-                        false => [list[index-1], *y, left].iter().min().unwrap() + 1
+                    left = match longer.as_bytes()[x - 1] == shorter.as_bytes()[index - 1] {
+                        true => list[index - 1],
+                        false => [list[index - 1], *y, left].iter().min().unwrap() + 1,
                     };
                     temp.push(left);
                 }
+
+                // shortcircuit out 
                 if temp.iter().min().unwrap() > &distance {
-                    return (*string_in, distance + 1)
+                    return (*string_in, distance + 1);
                 }
                 list = temp
             }
@@ -71,12 +115,11 @@ pub fn spellcheck<'a>(dictionary_string: &'a str, word: &str, distance: usize) -
         })
         .filter(|x| x.1 < distance)
         .collect();
+    
+    // sort by distance and then return the words
     vec.par_sort_by(|a, b| a.1.cmp(&b.1));
-
     vec.par_iter().map(|x| x.0).collect()
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -92,9 +135,7 @@ mod tests {
     #[bench]
     fn bench_actual_dict(bench: &mut Bencher) {
         let dictionary_string = include_str!("words.txt");
-        bench.iter(||
-            crate::spellcheck(&dictionary_string, "restaraunt", 3)
-        )
+        bench.iter(|| crate::spellcheck(&dictionary_string, "restaraunt", 3))
     }
 
     #[test]
@@ -103,6 +144,9 @@ mod tests {
 thin
 thing
 ";
-        assert_eq!(crate::spellcheck(string, "thinga", 3), vec!["thing", "thin"])
+        assert_eq!(
+            crate::spellcheck(string, "thinga", 3),
+            vec!["thing", "thin"]
+        )
     }
 }
