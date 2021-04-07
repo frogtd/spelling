dc64da59e69c4bdd9403d97b4fefcc31b4037125
frogtd <31412003+frogtd@users.noreply.github.com>
2021-04-06 22:42:29 -0400

    remove testing binary

--- src/test_bin.rs
+++
@@ -1,8 +0,0 @@
-use spelling::spellcheck;
-
-fn main() {
-    let dictionary_string = include_str!("words.txt");
-    for _ in 0..100 {
-        spellcheck(dictionary_string, "restaraunt", 3);
-    }
-}
